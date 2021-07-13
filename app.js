ff32a1a0064a14e98ce7888ce896930531e67ea7
MakAndJo <makandjo@outlook.com>
2021-07-14 01:13:29 +0500

    update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express'),
+const express = require('express'),
 	bodyParser = require('body-parser'),
 	OAuth2Server = require('oauth2-server'),
 	Request = OAuth2Server.Request,
 	Response = OAuth2Server.Response;
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -25,25 +25,19 @@ app.get('/', authenticateRequest, function (req, res) {
 app.listen(3000);
 
 function obtainToken(req, res) {
-
-	var request = new Request(req);
-	var response = new Response(res);
-
+	let request = new Request(req);
+	let response = new Response(res);
 	return app.oauth.token(request, response)
-		.then(function(token) {
-
-			res.json(token);
-		}).catch(function(err) {
-
-			res.status(err.code || 500).json(err);
-		});
+		.then((token) => 
+			res.json(token)
+		).catch((err) => 
+			res.status(err.code || 500).json(err)
+		);
 }
 
 function authenticateRequest(req, res, next) {
-
-	var request = new Request(req);
-	var response = new Response(res);
-
+	let request = new Request(req);
+	let response = new Response(res);
 	return app.oauth.authenticate(request, response)
 		.then(function (token) {
 			console.log('Accessed with token:', token);
